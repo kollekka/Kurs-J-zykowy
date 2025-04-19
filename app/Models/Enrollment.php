92aa025927e5702430694082b226b6dfad064981
Adrian <adrianswieca491@gmail.com>
2025-04-19 21:01:30 +0200

    relations

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    public function instructor()
+    {
+        return $this->belongsTo(Instructor::class);
+    }
+
+public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
+}
