105d807de359f6d282be90a5b32e9398cf633086
kollekka <adrianswieca145@gmail.com>
2025-04-21 18:02:07 +0200

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,17 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'adrian@gmail.com',
         //     'password' => bcrypt('1234'),
         // ]);  
+        // User::factory()->create([
+        //     'name' => 'Tomek',
+        //     'email' => 'tomek@gmail.com',
+        //     'password' => bcrypt('1234'),
+        // ]);  
              
-        $this->call([
-        //  InstructorSeeder::class,
-        //  CourseSeeder::class,
-        // EnrollmentSeeder::class,
-            PaymentSeeder::class,
-        ]);
+        // $this->call([
+        //     InstructorSeeder::class,
+        //     CourseSeeder::class,
+        //     EnrollmentSeeder::class,
+        //     PaymentSeeder::class,
+        // ]);
     }
 }
--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -12,7 +12,7 @@ class PaymentSeeder extends Seeder
     {
         DB::table('payments')->insert([
             [
-                'enrollment_id' => 3, // Zakładamy, że zapis o ID 1 istnieje
+                'enrollment_id' => 1, // Zakładamy, że zapis o ID 1 istnieje
                 'amount' => 199.99,
                 'payment_date' => now(),
                 'payment_method' => 'card',
@@ -21,7 +21,7 @@ class PaymentSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'enrollment_id' => 4, // Zakładamy, że zapis o ID 2 istnieje
+                'enrollment_id' => 2, // Zakładamy, że zapis o ID 2 istnieje
                 'amount' => 299.99,
                 'payment_date' => now(),
                 'payment_method' => 'paypal',
